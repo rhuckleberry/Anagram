162640b5fbd8ff6185b726133e746d3a4cdc37df
Ryan Huckleberry <ryanthuckleberry@gmail.com>
2020-11-25 12:34:19 -0800

    Main

--- src/main/Main.java
+++ src/main/Main.java
@@ -13,10 +13,27 @@ public class Main {
     public static void main(String[] args) throws IOException {
         //wordlist path
         String userDir = System.getProperty("user.dir");
-        String wordListPath = userDir + "/src/";
+        String wordListPath = userDir + "/src/wordlists/wordlist.txt";
 
         //build trie
         CompTrie builtTrie = CompTrieBuilder.buildCompTrie(wordListPath);
+
+        System.out.println(builtTrie.permuteContains("hippo"));
+    }
+
+    public void run() {
+        //wordlist path
+        String userDir = System.getProperty("user.dir");
+        String wordListPath = userDir + "/src/main/wordlists/wordlist.txt";
+
+        //build trie
+        CompTrie builtTrie;
+        try {
+            builtTrie = CompTrieBuilder.buildCompTrie(wordListPath);
+        } catch (IOException e){
+            System.out.println("[Error] Invalid Input Path");
+            return;
+        }
 
         System.out.println(builtTrie.permuteContains("hippo"));
     }
---
+++ src/test/MainTest.java
@@ -0,0 +1,15 @@
+package test;
+
+import main.Main;
+import org.junit.Test;
+
+import java.io.IOException;
+
+public class MainTest {
+
+    @Test
+    public void run(){
+        Main main = new Main();
+        main.run();
+    }
+}
